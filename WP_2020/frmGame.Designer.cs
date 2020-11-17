144ed1a918fb9dacfd4d200729c525cd9970c307
shuoya <timehet556947@gmail.com>
2020-11-17 17:46:13 +0800

    完成

--- WP_2020/frmGame.Designer.cs
+++ WP_2020/frmGame.Designer.cs
@@ -31,24 +31,24 @@
             this.btnDraw = new System.Windows.Forms.Button();
             this.picResult = new System.Windows.Forms.PictureBox();
             this.rtbLog = new System.Windows.Forms.RichTextBox();
-            this.labLog = new System.Windows.Forms.Label();
+            this.bt_reset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
             this.SuspendLayout();
             // 
             // btnDraw
             // 
             this.btnDraw.Font = new System.Drawing.Font("新細明體", 40F);
-            this.btnDraw.Location = new System.Drawing.Point(343, 69);
+            this.btnDraw.Location = new System.Drawing.Point(343, 12);
             this.btnDraw.Name = "btnDraw";
             this.btnDraw.Size = new System.Drawing.Size(250, 80);
             this.btnDraw.TabIndex = 0;
-            this.btnDraw.Text = "GET";
+            this.btnDraw.Text = "抽牌";
             this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
             // 
             // picResult
             // 
-            this.picResult.Image = global::WP_2020.Properties.Resources.TEST;
-            this.picResult.Location = new System.Drawing.Point(343, 174);
+            this.picResult.Location = new System.Drawing.Point(343, 188);
             this.picResult.Name = "picResult";
             this.picResult.Size = new System.Drawing.Size(250, 250);
             this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -57,36 +57,37 @@
             // 
             // rtbLog
             // 
-            this.rtbLog.Location = new System.Drawing.Point(39, 69);
+            this.rtbLog.Location = new System.Drawing.Point(39, 12);
             this.rtbLog.Name = "rtbLog";
-            this.rtbLog.Size = new System.Drawing.Size(258, 369);
+            this.rtbLog.Size = new System.Drawing.Size(258, 426);
             this.rtbLog.TabIndex = 2;
             this.rtbLog.Text = "";
             // 
-            // labLog
+            // bt_reset
             // 
-            this.labLog.AutoSize = true;
-            this.labLog.Font = new System.Drawing.Font("新細明體", 40F);
-            this.labLog.Location = new System.Drawing.Point(30, 12);
-            this.labLog.Name = "labLog";
-            this.labLog.Size = new System.Drawing.Size(144, 54);
-            this.labLog.TabIndex = 3;
-            this.labLog.Text = "TEST";
+            this.bt_reset.Font = new System.Drawing.Font("新細明體", 40F);
+            this.bt_reset.Location = new System.Drawing.Point(343, 98);
+            this.bt_reset.Name = "bt_reset";
+            this.bt_reset.Size = new System.Drawing.Size(250, 80);
+            this.bt_reset.TabIndex = 4;
+            this.bt_reset.Text = "洗牌";
+            this.bt_reset.UseVisualStyleBackColor = true;
+            this.bt_reset.Click += new System.EventHandler(this.bt_reset_Click);
             // 
             // frmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labLog);
+            this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.bt_reset);
             this.Controls.Add(this.rtbLog);
             this.Controls.Add(this.picResult);
             this.Controls.Add(this.btnDraw);
             this.Name = "frmGame";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmGame_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,7 +96,7 @@
         private System.Windows.Forms.Button btnDraw;
         private System.Windows.Forms.PictureBox picResult;
         private System.Windows.Forms.RichTextBox rtbLog;
-        private System.Windows.Forms.Label labLog;
+        private System.Windows.Forms.Button bt_reset;
     }
 }
 
--- WP_2020/frmGame.cs
+++ WP_2020/frmGame.cs
@@ -7,14 +7,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WP_2020.Properties;
 
 namespace WP_2020
 {
     public partial class frmGame : Form
     {
+        List<Image> list = new List<Image>();
+        Poker p = new Poker();
+        List<int> poker;
+        int click;
         public frmGame()
         {
             InitializeComponent();
+        }
+
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            #region 加入图片
+            list.Add(Resources._000);
+            list.Add(Resources.AC);
+            list.Add(Resources.AD);
+            list.Add(Resources.AH);
+            list.Add(Resources.AS);
+            list.Add(Resources._2C);
+            list.Add(Resources._2D);
+            list.Add(Resources._2H);
+            list.Add(Resources._2S);
+            list.Add(Resources._3C);
+            list.Add(Resources._3D);
+            list.Add(Resources._3H);
+            list.Add(Resources._3S);
+            list.Add(Resources._4C);
+            list.Add(Resources._4D);
+            list.Add(Resources._4H);
+            list.Add(Resources._4S);
+            list.Add(Resources._5C);
+            list.Add(Resources._5D);
+            list.Add(Resources._5H);
+            list.Add(Resources._5S);
+            list.Add(Resources._6C);
+            list.Add(Resources._6D);
+            list.Add(Resources._6H);
+            list.Add(Resources._6S);
+            list.Add(Resources._7C);
+            list.Add(Resources._7D);
+            list.Add(Resources._7H);
+            list.Add(Resources._7S);
+            list.Add(Resources._8C);
+            list.Add(Resources._8D);
+            list.Add(Resources._8H);
+            list.Add(Resources._8S);
+            list.Add(Resources._9C);
+            list.Add(Resources._9D);
+            list.Add(Resources._9H);
+            list.Add(Resources._9S);
+            list.Add(Resources._10C);
+            list.Add(Resources._10D);
+            list.Add(Resources._10H);
+            list.Add(Resources._10S);
+            list.Add(Resources.JC);
+            list.Add(Resources.JD);
+            list.Add(Resources.JH);
+            list.Add(Resources.JS);
+            list.Add(Resources.KC);
+            list.Add(Resources.KD);
+            list.Add(Resources.KH);
+            list.Add(Resources.KS);
+            list.Add(Resources.QC);
+            list.Add(Resources.QD);
+            list.Add(Resources.QH);
+            list.Add(Resources.QS);
+            #endregion
+
+            picResult.Image = list[0];
+            poker = p.GetPoker(52);
+            click = 0;
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (click < 52)
+            {
+                rtbLog.Text = "里面还剩下 " + (51-click).ToString() + " 张";
+                picResult.Image = list[poker[click]+1];
+                click += 1;
+            }
+            else
+            {
+                MessageBox.Show("抽完了");
+            }
+        }
+
+        private void bt_reset_Click(object sender, EventArgs e)
+        {
+            click = 0;
+            poker = p.GetPoker(52);
+            picResult.Image = list[0];
+            rtbLog.Text = "";
         }
     }
 }
